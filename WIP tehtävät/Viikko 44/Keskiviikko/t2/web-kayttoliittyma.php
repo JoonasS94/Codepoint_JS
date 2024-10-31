34728d5f90d2ba4c0a60f881af8aafd64487d6d3
Joonas Sariola <91365380+JoonasS94@users.noreply.github.com>
2024-10-31 14:39:04 +0200

    All tables have name/ date printed to php-site
    All previously made three tables (reserves, reserver's and date of reserve) have reserve-place, reserver's name and date of reserve printing to website.
    
    Joonas Sariola / 31.10.2024 14:38

--- WIP tehtävät/Viikko 44/Keskiviikko/t2/web-kayttoliittyma.php
+++ WIP tehtävät/Viikko 44/Keskiviikko/t2/web-kayttoliittyma.php
@@ -7,29 +7,114 @@
 </head>
 <body>
 
-<?php 
+<?php
+    //Login details to access SQL-server.
     $host= "localhost"; 
     $username = "root"; 
     $database = "tilavaraus"; 
-    $password = ""; 
-    try { 
+    $password = "";
+    //Attempt to login.
+    try
+    { 
         $yhteys = new PDO("mysql:host=$host;dbname=$database", $username, $password); 
         $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     } 
+    //Error message if failure in login details.
     catch(PDOException $e){ echo "<p>".$e->getMessage()."<p>"; } 
-    $sql_lause =  "SELECT * FROM tilat"; 
-    try { 
+    
+    //Login in to server. 
+    try
+    { 
+        $yhteys = new PDO("mysql:host=$host;dbname=$database", $username, $password); 
+        $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    }
+    //Error message if failing to login.
+    catch(PDOException $e){ echo "<p>".$e->getMessage()."<p>"; } 
+    
+
+
+
+
+
+    //Get all names from tilat table
+    $sql_lause = "SELECT * FROM tilat"; 
+    try
+    { 
         $query= $yhteys->prepare($sql_lause); 
         $query->execute(); 
-    }  
+    }
+    //Error message if failing to get all names from tilat table.
     catch (PDOException $e) { die("VIRHE: " . $e->getMessage()); } 
-    $result= $query->fetchAll();
- 
+
+    $result = $query->fetchAll();
+
+    //Print names from tilat table.
+    echo "<ul>";
+    foreach($result as $tila)
+    { 
+        echo "<li>" . $tila['tilan_nimi'] . "</li>"; 
+    }
+    echo "</ul>";
+
+
+
+
+
+
+
+
+    //Get all names from varaajat table
+    $sql_lause = "SELECT * FROM varaajat"; 
+    try
+    { 
+        $query= $yhteys->prepare($sql_lause); 
+        $query->execute(); 
+    }
+    //Error message if failing to get all names from varaajat table.
+    catch (PDOException $e) { die("VIRHE: " . $e->getMessage()); } 
+
+    $result = $query->fetchAll();
+
+    //Print names from varaajat table.
+    echo "<ul>";
+    foreach($result as $tila)
+    { 
+        echo "<li>" . $tila['varaajan_nimi'] . "</li>"; 
+    }
+    echo "</ul>";
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Get all names from varaukset table
+    $sql_lause = "SELECT * FROM varaukset"; 
+    try
+    { 
+        $query= $yhteys->prepare($sql_lause); 
+        $query->execute(); 
+    }
+    //Error message if failing to get all dates from varaukset table.
+    catch (PDOException $e) { die("VIRHE: " . $e->getMessage()); } 
+
+    $result = $query->fetchAll();
+
+    //Print names from varaajat table.
+    echo "<ul>";
+    foreach($result as $varaukset)
+    { 
+        echo "<li>" . $varaukset['varauspaiva'] . "</li>"; 
+    }
+    echo "</ul>";
 ?>
-    <h1>Tervetuloa!</h1>;
 
-
-
+<h1>Tervetuloa!</h1>
 
 </body>
 </html>
